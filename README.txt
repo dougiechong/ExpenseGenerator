0b6186a034e9047f05dc808292e3643890b99cae
Douglas <dougiechong@gmail.com>
2018-01-05 15:21:12 -0500

    added first revision, needs error checking, and titles

--- README.txt
+++ README.txt
@@ -8,4 +8,4 @@ Python 2
 
 How to run:
 
-Run the command python expense_generator from the folder containing all your invoices named in the following format mmddyy-description-vendor-dollars_cents, where mm=month (01-12), dd=day(01-31), yy=(00-99), description is a description of the invoice, dollars=dollars associated with invoice, cens=cents associated with invoice
+Run the command "python expense_generator name_of_csv" from the folder containing all your invoices named in the following format mmddyy-description-vendor-dollars_cents, where mm=month (01-12), dd=day(01-31), yy=(00-99), description is a description of the invoice, dollars=dollars associated with invoice, cens=cents associated with invoice. name_of_csv is the name of the csv file you wish to generate
---
+++ TotalExpenseGenerator.py
@@ -0,0 +1,55 @@
+###################################################################### 
+### Description:
+### 
+### This program will create an expense csv from a 
+### directory with appropriately named invoices
+###
+### Prerequisites:
+###
+### Python 2
+###
+### How to run:
+###
+### Run the command "python expense_generator name_of_csv path"
+### where path contains all your invoices named in the following
+### format mmddyy-description-vendor-dollars_cents, 
+### where mm=month (01-12), dd=day(01-31), yy=(00-99), description 
+### is a description of the invoice, dollars=dollars associated with 
+### invoice, cens=cents associated with invoice. name_of_csv is the 
+### name of the csv file you wish to generate.
+########################################################################
+
+import sys
+import string
+from os import listdir
+from os.path import isfile, join
+
+if(len(sys.argv) > 2):
+    mypath = sys.argv[2]
+    receipts = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+else:
+    receipts = [f for f in listdir('.') if isfile(f)]
+
+#open the output file to write
+output_file = sys.argv[1]
+output_file = output_file + '.csv'
+f = open(output_file, 'w')
+
+for r in receipts:
+    #strip the file extension from the name
+    strip_extension = r.split(".")[0]
+    #replace dashes with commas
+    comma_separated = string.replace(strip_extension, '-', ',')
+    #replace _ with .
+    result = string.replace(comma_separated, '_', '.')
+    f.write(result)
+    f.write('\n')
+    #print result
+
+summation = len(receipts)
+f.write(',,,=SUM(D1:D%d)' % (summation))
+#close the file
+f.close()
+    
+
+
